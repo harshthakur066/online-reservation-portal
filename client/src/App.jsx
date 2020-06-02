e6d7c7504d182f00c00888a8722260b62c548808
harshthakur066 <harshthakur066@gmail.com>
2020-06-02 14:26:25 +0530

    added header and css

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Container>
+        <Header />
+      </Container>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <div className="nav-wrapper light-blue darken-4">
+          <div className="brand-logo center" style={{ cursor: "pointer" }}>
+            Online Reservation Portal
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
