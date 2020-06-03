4eb4420b5243c83d0b318b372f3772f3b6426b7a
harshthakur066 <harshthakur066@gmail.com>
2020-06-03 11:57:21 +0530

    changed landing state to hooks

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
@@ -6,78 +6,81 @@ import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 
 import "./Landing.css";
 
-class Landing extends React.Component {
-  constructor() {
-    super();
+const Landing = () => {
+  // constructor() {
+  //   super();
 
-    this.state = {
-      currentUser: null,
-    };
-  }
+  //   this.state = {
+  //     currentUser: null,
+  //   };
+  // }
 
-  unsubscribeFromAuth = null;
+  const [currentUser, setCurrentUser] = useState(null);
 
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+  let unsubscribeFromAuth = null;
+
+  const isUser = () => {
+    unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
         });
       }
 
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
-  }
+  };
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-  render() {
-    const { currentUser } = this.state;
-    console.log(currentUser);
-    return (
-      <div>
-        <div className="text">
-          Facility Booking is an online reservation portal for customers who
-          wants different facilities and the providers who are looking for
-          customers.
-          <br />
-          <br />
-          Customers can fill up the form and mention that which type of facility
-          they are looking for as we provide facilites.
-          <br />
-          <br />
-          Faccility providers can login to thier to the account and can have
-          thier customers details.
+  useEffect(() => {
+    isUser();
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    return () => unsubscribeFromAuth();
+    // eslint-disable-next-line
+  }, []);
+
+  console.log(currentUser);
+  return (
+    <div>
+      <div className="text">
+        Facility Booking is an online reservation portal for customers who wants
+        different facilities and the providers who are looking for customers.
+        <br />
+        <br />
+        Customers can fill up the form and mention that which type of facility
+        they are looking for as we provide facilites.
+        <br />
+        <br />
+        Faccility providers can login to thier to the account and can have thier
+        customers details.
+      </div>
+      <div className="display">
+        <div className="view">
+          <Link to="/customer/form">
+            <Button primary className=" btn-large ">
+              For Customers
+            </Button>
+          </Link>
         </div>
-        <div className="display">
-          <div className="view">
-            <Link to="/customer/form">
-              <Button primary className=" btn-large ">
-                For Customers
-              </Button>
-            </Link>
-          </div>
-          <div className="view">
-            <Link
-              to={currentUser ? "/provider/getcustomers" : "/provider/signin"}
-            >
-              <Button primary className="btn-large ">
-                For Facility Providers
-              </Button>
-            </Link>
-          </div>
+        <div className="view">
+          <Link
+            to={currentUser ? "/provider/getcustomers" : "/provider/signin"}
+          >
+            <Button primary className="btn-large ">
+              For Facility Providers
+            </Button>
+          </Link>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Landing;
